5c5b5b3e73e2ab8798c539b94a0a27721e34e9dd
Alan Kha <akhahaha@gmail.com>
2016-09-14 14:52:49 -0700

    Add annotations

---
+++ src/main/java/firebomb/annotation/OneToMany.java
@@ -0,0 +1,12 @@
+package firebomb.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OneToMany {
+    String foreignFieldName();
+}
