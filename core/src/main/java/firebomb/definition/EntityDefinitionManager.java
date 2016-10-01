caa4b06fd5dbebb66cf14d491a0ead4b34d76e68
Alan Kha <akhahaha@gmail.com>
2016-10-01 00:50:36 -0700

    Update EntityDefinition caching

---
+++ core/src/main/java/firebomb/definition/EntityDefinitionManager.java
@@ -0,0 +1,38 @@
+package firebomb.definition;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntityDefinitionManager {
+    private static EntityDefinitionManager ourInstance = new EntityDefinitionManager();
+
+    public static EntityDefinitionManager getInstance() {
+        return ourInstance;
+    }
+
+    private Map<Class, BasicEntityDefinition> basicDefinitionMap = new HashMap<>();
+    private Map<Class, EntityDefinition> definitionMap = new HashMap<>();
+
+    public BasicEntityDefinition getBasicDefinition(Class entityType) {
+        if (definitionMap.containsKey(entityType)) {
+            return definitionMap.get(entityType);
+        }
+
+        if (!basicDefinitionMap.containsKey(entityType)) {
+            basicDefinitionMap.put(entityType, new BasicEntityDefinition(entityType));
+        }
+
+        return basicDefinitionMap.get(entityType);
+    }
+
+    public EntityDefinition getDefinition(Class entityType) {
+        if (!definitionMap.containsKey(entityType)) {
+            definitionMap.put(entityType, new EntityDefinition(entityType));
+            if (basicDefinitionMap.containsKey(entityType)) {
+                basicDefinitionMap.remove(entityType);
+            }
+        }
+
+        return definitionMap.get(entityType);
+    }
+}
